904c8c7838e4ef3c2a52783d5b1090949bcf7627
Angelo Guilhermino <angeloguilhermino@gmail.com>
2022-11-29 01:57:30 -0300

    add springSecurity

--- src/main/java/com/api/patientApi/PatientApiApplication.java
+++ src/main/java/com/api/patientApi/PatientApiApplication.java
@@ -2,6 +2,7 @@ package com.api.patientApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class PatientApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PatientApiApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("123456"));
 	}
 
 	@GetMapping("/")
